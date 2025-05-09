f4cf9d6b81771640be6f7865bc2885a9c824fd7d
Kevin Suñer <kevinsuner99@gmail.com>
2025-05-09 22:16:17 +0200

    Update odin path, add golang to path

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 set -g fish_greeting "Do cool shit."
 set PATH $PATH ~/.local/bin
-set PATH $PATH ~/odin
+set PATH $PATH /usr/local/odin
+set PATH $PATH /usr/local/go/bin
 set EDITOR "vim"
 
 fish_ssh_agent
