3e0e89b9afc4f5915e21d83a1173fe8f0ce5698d
Kevin Suñer <kevinsuner99@gmail.com>
2025-05-12 21:43:50 +0200

    Set nvim as default editor

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,6 @@ set -g fish_greeting "Do cool shit."
 set PATH $PATH ~/.local/bin
 set PATH $PATH /usr/local/odin
 set PATH $PATH /usr/local/go/bin
-set EDITOR "vim"
+set EDITOR "nvim"
 
 fish_ssh_agent
